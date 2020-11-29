2796b816d026eb07efd16cffac732aefe097b834
ezyjay <esmealicejames@gmail.com>
2020-11-29 18:19:48 -0500

    Added possibility to build vertically on top of other blocks + adapted preview

--- Assets/Scripts/GridDrawer.cs
+++ Assets/Scripts/GridDrawer.cs
@@ -6,14 +6,105 @@ public class GridDrawer : MonoBehaviour
 {
     public int maxGridSize = 10;
     public int tilesize = 1;
-    public SpriteRenderer tile, previewSprite;
+    public SpriteRenderer tile;
 
-  
-    public void MovePreviewSprite(Vector3 position) {
-        
-        float newXPos = tilesize * Mathf.Round(position.x / tilesize);
-        float newZPos = tilesize * Mathf.Round(position.z / tilesize);
-        Vector3 newPos = new Vector3(newXPos, previewSprite.transform.position.y, newZPos) + new Vector3(1, 0, 1) * tilesize / 2;
-        previewSprite.transform.position = newPos;
+    private Vector3 offset;
+    //Tracks the number of objects contained in each tile of the grid
+    private int[,] grid;
+
+    public class GridIndex
+    {
+        public int x;
+        public int y;
+
+        public GridIndex(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+    }
+
+    private void Awake() {
+
+        ClearGrid(); 
+
+        float xOffset = transform.position.x % 1;
+
+        if (xOffset > 0.5f)
+            xOffset -= tilesize;
+
+        float zOffset = transform.position.z % 1;
+        offset = new Vector3(xOffset, 0, zOffset);
+    }
+
+    /// <summary>
+    /// Tranform the position of an object into a index on the grid
+    /// </summary>
+    /// <param name="position"> The position of an object </param>
+    /// <returns> A GridIndex containing the x and y index of the tile </returns>
+    public GridIndex GetTileIndexFromPosition(Vector3 position) {
+        return new GridIndex((int)position.x, (int)position.z);
+    }
+
+    /// <summary>
+    /// Clears the grid
+    /// </summary>
+    public void ClearGrid() {
+        grid = new int[maxGridSize, maxGridSize];
+    }
+
+    /// <summary>
+    /// Increment the number of objects at index
+    /// </summary>
+    /// <param name="gridIndex"> The index of the tile </param>
+    public void AddObjectToGrid(GridIndex gridIndex) {
+
+        grid[gridIndex.x, gridIndex.y] += 1;
+        Debug.Log("Object Add to grid at index " + gridIndex.x + ", " + gridIndex.y + ": Number of objects in tile = " + grid[gridIndex.x, gridIndex.y]);
+    }
+
+    /// <summary>
+    /// Checks the number of objects in a tile and returns true if there are more than 0
+    /// </summary>
+    /// <param name="gridIndex"> The index of the tile </param>
+    /// <returns> A bool indicating wether the tile contains an object of not </returns>
+    public bool HasObjectInTile(GridIndex gridIndex) {
+
+        return grid[gridIndex.x, gridIndex.y] > 0;
+    }
+
+    /// <summary>
+    /// Gets the number of objects in a tile
+    /// </summary>
+    /// <param name="gridIndex"> The index of the tile </param>
+    /// <returns> The numbers of objects in the tile </returns>
+    public int GetNumberOfObjectsInTile(GridIndex gridIndex) {
+
+        return grid[gridIndex.x, gridIndex.y];
+    }
+
+    /// <summary>
+    /// Moves an object on the grid
+    /// </summary>
+    /// <param name="objectTransform"> The object to move</param>
+    /// <param name="newPosition"> The new position of the object </param>
+    public void MoveObjectOnGrid(Transform objectTransform, Vector3 newPosition) {
+        float newXPos = tilesize * Mathf.Round(newPosition.x / tilesize);
+        float newZPos = tilesize * Mathf.Round(newPosition.z / tilesize);
+        Vector3 newPos = new Vector3(newXPos, objectTransform.position.y, newZPos) + new Vector3(1, 0, 1) * tilesize / 2 + offset;
+        objectTransform.position = newPos;
+    }
+
+    /// <summary>
+    /// Checks if the position is inside the grid
+    /// </summary>
+    /// <param name="position"> The position of the object </param>
+    /// <returns> A bool indicating wether the position is inside the bounds of the grid </returns>
+    public bool IsPositionInGrid(Vector3 position) {
+
+        position += offset;
+        if (position.x > maxGridSize || position.z > maxGridSize || position.x < 0 || position.z < 0)
+            return false;
+
+        return true;
     }
 }
--- Assets/Scripts/SandcastleManager.cs
+++ Assets/Scripts/SandcastleManager.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using Clayxels;
+using static GridDrawer;
 
 public class SandcastleManager : MonoBehaviour
 {
@@ -7,12 +8,25 @@ public class SandcastleManager : MonoBehaviour
     public ClayContainer clayContainer;
     public SandBlobParams sandBlobParameters;
     public GridDrawer gridDrawer;
+    public ClayObject sandBlobPreview;
     
     private RaycastHit hit;
     private Ray ray;
 
+    [EditorButton]
+    public void ResetBuildZone() {
+        int i = 0;
+        foreach (Transform clayObject in clayContainer.transform) {
+            //Don't destroy 2 first children because first one is base and second is preview
+            if (i > 1) {
+                Destroy(clayObject.gameObject);
+            }
+            i++;
+        }
+        gridDrawer.ClearGrid();
+    }
 
-[System.Serializable]
+    [System.Serializable]
     public struct SandBlobParams {
         public Vector3 size;
         public Color color;
@@ -20,31 +34,72 @@ public class SandcastleManager : MonoBehaviour
         public float round;
         public int primitiveType;
     }
-    
+
+    private void OnEnable() {
+        clayContainer.enableAllClayObjects(true);
+        //Disable base clay object so it doesn't update every frame
+        clayContainer.GetComponentInChildren<ClayObject>().enabled = false;
+        sandBlobPreview.gameObject.SetActive(false);
+    }
+
     private void Update()
     {
         if (isInBuildMode) {
 
+            //Ray cast down and if it's a buildable area, spawn sand blob on mouse click
             ray = Camera.main.ScreenPointToRay(Input.mousePosition);
             if (Physics.Raycast(ray, out hit)) {
-                gridDrawer.previewSprite.enabled = true;
-                gridDrawer.MovePreviewSprite(hit.point);
+
+                gridDrawer.MoveObjectOnGrid(sandBlobPreview.transform, hit.point);
+                if (gridDrawer.IsPositionInGrid(sandBlobPreview.transform.position)) {
+
+                    //Change position of preview object
+                    GridIndex gridIndex = gridDrawer.GetTileIndexFromPosition(sandBlobPreview.transform.position);
+                    Vector3 spawnPosition = new Vector3(sandBlobPreview.transform.position.x, transform.position.y, sandBlobPreview.transform.position.z);
+
+                    if (gridDrawer.HasObjectInTile(gridIndex)) {
+                        float newYPosition = spawnPosition.y + gridDrawer.GetNumberOfObjectsInTile(gridIndex) * gridDrawer.tilesize;
+                        spawnPosition = new Vector3(spawnPosition.x, newYPosition, spawnPosition.z);
+                        sandBlobPreview.transform.position = new Vector3(sandBlobPreview.transform.position.x, spawnPosition.y - .6f * gridDrawer.tilesize, sandBlobPreview.transform.position.z);
+                    }
+                    else {
+                        sandBlobPreview.transform.position = new Vector3(sandBlobPreview.transform.position.x, transform.position.y, sandBlobPreview.transform.position.z);
+                    }
+
+                    sandBlobPreview.gameObject.SetActive(true);
+
+                    //Add sand blobl on click
+                    if (Input.GetMouseButtonDown(0)) 
+                        AddSandBlob(gridIndex, spawnPosition);
+                }
+                else {
+                    clayContainer.enableAllClayObjects(true);
+                    sandBlobPreview.gameObject.SetActive(false);
+                }
             }
             else {
-                gridDrawer.previewSprite.enabled = false;
-            }
-
-            if (Input.GetMouseButtonDown(0)) {
-
-                
-                ClayObject clayObject = clayContainer.addClayObject();
-                InitializeBlob(clayObject, sandBlobParameters, gridDrawer.previewSprite.transform.position);
                 clayContainer.enableAllClayObjects(true);
+                sandBlobPreview.gameObject.SetActive(false);
             }
+
         }
     }
 
-    private void InitializeBlob(ClayObject clayObject, SandBlobParams parameters, Vector3 position) {
+    private void AddSandBlob(GridIndex gridIndex, Vector3 spawnPosition) {
+
+        //Creat clay object
+        ClayObject clayObject = clayContainer.addClayObject();
+        
+        //Initalize blob with parameters
+        InitializeSandBlob(clayObject, sandBlobParameters, spawnPosition);
+        clayObject.enabled = false;
+        gridDrawer.AddObjectToGrid(gridIndex);
+        sandBlobPreview.transform.SetAsLastSibling();
+        clayContainer.forceUpdateAllSolids(); 
+        clayContainer.enableAllClayObjects(true);
+    }
+
+    private void InitializeSandBlob(ClayObject clayObject, SandBlobParams parameters, Vector3 position) {
         clayObject.transform.localScale = parameters.size;
         clayObject.color = parameters.color;
         clayObject.blend = parameters.blend/100;
